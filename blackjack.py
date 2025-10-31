e0705a06f1ae70c3e05909d41406ee966a51118e
ZachariahConyngham <zachariah.conyngham@education.nsw.gov.au>
2025-10-31 14:18:47 +1100

    blackjack

---
+++ blackjack.py
@@ -0,0 +1,113 @@
+import random
+facecards = ["Jack", "Queen", "King"]
+cardsnumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+Cards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+Suits = ["Diamonds", "Spades", "Hearts", "Clubs"]
+
+wins=0
+dealertotal = 0
+#while wins < 10:
+hit = "Y"
+print("The dealer has:")
+if hit == "Y":
+    cardnumber = random.choice(cardsnumber)
+    dealertotal += cardnumber
+    match cardnumber:
+        case 1:
+            facecard = "Ace"
+        case 2:
+            facecard = "2"
+        case 3:
+            facecard = "3"
+        case 4:
+            facecard = "4"
+        case 5:
+            facecard = "5"
+        case 6:
+            facecard = "6"
+        case 7:
+            facecard = "7"
+        case 8:
+            facecard = "8"
+        case 9:
+            facecard = "9"
+        case 10:
+            facecard = random.choice(facecards)
+    print(facecard, "of", random.choice(Suits))
+
+
+print("Your cards are:")
+total = 0
+facecard = 0
+while hit == "Y":
+    cardnumber = random.choice(cardsnumber)
+    total += cardnumber
+    match cardnumber:
+        case 1:
+            facecard = "Ace"
+        case 2:
+            facecard = "2"
+        case 3:
+            facecard = "3"
+        case 4:
+            facecard = "4"
+        case 5:
+            facecard = "5"
+        case 6:
+            facecard = "6"
+        case 7:
+            facecard = "7"
+        case 8:
+            facecard = "8"
+        case 9:
+            facecard = "9"
+        case 10:
+            facecard = random.choice(facecards)
+    print(facecard, "of", random.choice(Suits))
+    print("Your current total is", total)
+    if total > 21:
+        print("You lose")
+    else:
+        hit = str(input("Do you want another card? (Y or N)"))
+
+while dealertotal < 17:
+    print("The Dealers next card is:")
+    cardnumber = random.choice(cardsnumber)
+    dealertotal += cardnumber
+    match cardnumber:
+        case 1:
+            facecard = "Ace"
+        case 2:
+            facecard = "2"
+        case 3:
+            facecard = "3"
+        case 4:
+            facecard = "4"
+        case 5:
+            facecard = "5"
+        case 6:
+            facecard = "6"
+        case 7:
+            facecard = "7"
+        case 8:
+            facecard = "8"
+        case 9:
+            facecard = "9"
+        case 10:
+            facecard = random.choice(facecards)
+    print(facecard, "of", random.choice(Suits))
+
+
+
+
+
+if hit == "Y":
+    print(random.choice(Cards), "of", random.choice(Suits))
+else:
+    print("Your total is", total)
+    print("The dealers total is", dealertotal)
+    if dealertotal < total:
+        print("You win")
+        wins += 1
+    else:
+        print("you lose")
--- uno.py
+++ uno.py
@@ -7,6 +7,11 @@ Gcards = ["R-1", "R-2", "R-3", "R-4", "R-5", "R-6", "R-7", "R-8", "R-9", "R-10",
 Ycards = ["R-1", "R-2", "R-3", "R-4", "R-5", "R-6", "R-7", "R-8", "R-9", "R-10", "R+2", "R-skip", "R-reverse"]
 Blcards = ["B+4", "B-RBGY", "B+6", "B-reset", "B-swap"]
 
+numberofcards = 7
+entity3numberofcards = 7
+
+
+
 card1 = random.choice(Rcards)
 card2 = random.choice(Rcards)
 card3 = random.choice(Bcards)
